def51461dfb7043be97979393c1712c16b6f42df
Dusan Trtica <dtrtica@gogoair.com>
2016-02-17 11:12:15 +0100

    cool feature from 504 task

--- common/models/period.js
+++ common/models/period.js
@@ -1,3 +1,7 @@
 module.exports = function(Period) {
 	console.log('this is again some cool feature in dev, commit 2');
+	console.log('dev 504');
+	for(var i = 0; i < 10; i++){
+		console.log('task 504 cool feature');
+	}
 };
